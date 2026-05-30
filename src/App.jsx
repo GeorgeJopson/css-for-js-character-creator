950b500006b8563a14d119a499b526444309676f
George Jopson <georgejopson@gmail.com>
2026-05-30 12:16:47 +0100

    Fixed footer issues and update to improve padding

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,14 @@ import React from 'react';
 import CharacterEditor from './components/CharacterEditor';
 import Footer from './components/Footer';
 
+import styles from './App.module.css';
+
 function App() {
   return (
-    <>
+    <div className={styles.container}>
       <CharacterEditor />
       <Footer />
-    </>
+    </div>
   );
 }
 
